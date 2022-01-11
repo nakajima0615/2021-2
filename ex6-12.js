b6aef69d5df9b7bf59d709fde693ffc9b52c4ef9
nakajima0615 <zm20099@josai.ac.jp>
2022-01-11 02:06:01 +0000

    0111

--- ex6-12.js
+++ ex6-12.js
@@ -5,9 +5,9 @@ varstr ="一文字ずつ表示します。";
 var cnt=document.timer.moji.value.length;
 //文字が全部表示されているか確認 if (cnt<11){
 //現在より1文字多く切り出して表示
-document.timer.moji.value = str.substr(0,cnt+1);} else{
+document.timer2.moji.value = str.substr(0,cnt+1);} else{
 //全て表示されたら、空文字に戻す
-document.timer.moji.value="";}
+document.timer2.moji.value="";}
 //setTimeout()を含む関数を呼び出す
 setTimeout("hyoji()",1000);}
 
